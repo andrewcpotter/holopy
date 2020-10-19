7a2a5edd0f84629450fb8627e0529fe6a4e6c4e0
Drew <acpotter@utexas.edu>
2020-10-19 13:23:24 -0500

    defined IsoMPS class
    easier to create MPS, also added ParamCircuit object to hold circuit and parameters

--- test_debug.py
+++ test_debug.py
@@ -9,52 +9,44 @@ import numpy as np
 import qiskit as qk
 import networkx as nx
 
-from isonetwork import IsoTensor,IsoNetwork
+from isonetwork2 import*
 
 #%% qiskit debugging
 nphys = 2
 nbond = 2
-L = 3
+l_uc = 3
 
 # setup registers
 preg = qk.QuantumRegister(nphys,'p')
 breg = qk.QuantumRegister(nbond,'b')
 qregs = [preg,breg]
-cregs = [qk.ClassicalRegister(nphys,'m'+str(j)) for j in range(L)]
+cregs = [qk.ClassicalRegister(nphys,'m'+str(j)) for j in range(l_uc)]
 
 
 # setup variational parameters
 nvp = 3 # number of variational parameters
 plabels = [qk.circuit.Parameter('x'+str(j))for j in range(nvp)]
-pvals = 0.37*np.arange(L) 
+pvals = 0.37*np.arange(l_uc) 
 paramdict = dict(zip(plabels,pvals))
 
 # setup some arbitrary circuit for each node 
 # (same architecture for each node, different parameter value(s))
-circs=[qk.QuantumCircuit() for j in range(L)]
+
+# create list of circuits for each site
+circs=[qk.QuantumCircuit() for j in range(l_uc)]
 for j in [2,0,1]:#range(L):
     for reg in qregs: circs[j].add_register(reg)
     circs[j].cx(preg[0],breg[0])
     circs[j].rz(plabels[j],breg[0])
     circs[j].cx(preg[0],breg[0])
+    
+# convert to parameterized circuits
+pcircs = []
+for j in range(l_uc):
+    pcircs += [QKParamCircuit(circs[j],[plabels[j]])]
 
-# setup nodes and network
-meas_list = [[(preg,cregs[j],qk.QuantumCircuit())] for j in range(L)]
-nodes = [IsoTensor('n'+str(j),
-                   qregs,
-                   circs[j],
-                   [plabels[j]],
-                   meas_list[j], # list of measurements, specified as tuples
-                   circ_type='qiskit') for j in range(L)]
-edges = [
-        (nodes[0],nodes[1],{'qreg':breg}),
-        (nodes[1],nodes[2],{'qreg':breg})
-        ]
-#meas_dict = {nodes[j]:[(preg,cregs[j],qk.QuantumCircuit())] for j in range(len(cregs))}
-param_assignments = {nodes[j]:[plabels[j]] for j in range(L)}
-tn = IsoNetwork(nodes,edges,qregs,param_assignments)
+#%% HoloMPS
+psi_mps = IsoMPS(preg,breg,pcircs)
+psi_mps.construct_circuit(paramdict).draw('mpl',scale=0.5)
+print(psi_mps.to_qasm(paramdict))
 
-#  generate and draw the circuit
-tn.construct_circuit(paramdict).draw('mpl',scale=0.5)
-# output to qasm
-print(tn.to_qasm(paramdict))
